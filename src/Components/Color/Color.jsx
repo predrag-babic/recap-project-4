00af2523c3cf05f8a9d0ae7108eb300eebb5ccab
Predrag <predrag.babic@tactix-sports.com>
2024-08-19 12:24:10 +0200

    add cancel editing function to the color component

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./Color.css";
+import ColorForm from "../ColorForm";
 
-export default function Color({ color, onDeleteColor }) {
+export default function Color({ color, onDeleteColor, onUpdateColor }) {
   const [showConfirmText, setShowConfirmText] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -20,6 +21,15 @@ export default function Color({ color, onDeleteColor }) {
   function handleEditColor() {
     setIsEditing(true);
   }
+
+  function handleUpdateColor(updatedColor) {
+    onUpdateColor({ ...updatedColor, id: color.id });
+    setIsEditing(false);
+  }
+
+  function handleCancelEditing() {
+    setIsEditing(false);
+  }
   return (
     <div
       className="color-card"
@@ -28,21 +38,31 @@ export default function Color({ color, onDeleteColor }) {
         color: color.contrastText,
       }}
     >
-      <h3 className="color-card-headline ">{color.hex}</h3>
-      <h4>{color.role}</h4>
-      <p>contrast Text: {color.contrastText}</p>
-      {showConfirmText ? (
-        <div>
-          <p className="color-card-highlight">
-            Are you sure you want do delete this color?
-          </p>
-          <button onClick={handleConfirmDelete}>Yes</button>
-          <button onClick={handleCancelDelete}>No</button>
-        </div>
+      {isEditing ? (
+        <ColorForm
+          onSubmitColor={handleUpdateColor}
+          startData={color}
+          onCancel={handleCancelEditing}
+        />
       ) : (
         <>
-          <button onClick={handleDeleteColor}>Delete</button>
-          <button onClick={handleDeleteColor}>Edit</button>
+          <h3 className="color-card-headline ">{color.hex}</h3>
+          <h4>{color.role}</h4>
+          <p>contrast Text: {color.contrastText}</p>
+          {showConfirmText ? (
+            <div>
+              <p className="color-card-highlight">
+                Are you sure you want do delete this color?
+              </p>
+              <button onClick={handleConfirmDelete}>Yes</button>
+              <button onClick={handleCancelDelete}>No</button>
+            </div>
+          ) : (
+            <>
+              <button onClick={handleDeleteColor}>Delete</button>
+              <button onClick={handleEditColor}>Edit</button>
+            </>
+          )}
         </>
       )}
     </div>
