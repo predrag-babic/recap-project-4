763841770d83e2bf9abfb0b005174ccc8fdcf926
Predrag <predrag.babic@tactix-sports.com>
2024-08-20 14:35:04 +0200

    add useEffect hook to the component and set the timer

--- src/Components/CopyToClipboard.jsx
+++ src/Components/CopyToClipboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CopyToClipboard({ hex }) {
   const [isCopied, setIsCopied] = useState(false);
@@ -6,8 +6,16 @@ export default function CopyToClipboard({ hex }) {
   async function handleCopyToClipboard() {
     try {
       await navigator.clipboard.writeText(hex);
+      setIsCopied(true);
     } catch (error) {
       console.error("Failed to copy!", error);
     }
   }
+
+  useEffect(() => {
+    if (isCopied) {
+      const timer = setInterval(() => setIsCopied(false), 3000);
+      return () => clearInterval(timer);
+    }
+  }, [isCopied]);
 }
