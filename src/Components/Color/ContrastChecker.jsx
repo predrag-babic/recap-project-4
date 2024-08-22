4902a162cbfee700d1eea410ea31899dcc79b431
Predrag <predrag.babic@tactix-sports.com>
2024-08-22 11:03:21 +0200

    reworked fetch api function

--- src/Components/Color/ContrastChecker.jsx
+++ src/Components/Color/ContrastChecker.jsx
@@ -5,20 +5,23 @@ export default function ContrastChecker({ hex, contrastText }) {
 
   useEffect(() => {
     async function postFetch() {
-      fetch("https://www.aremycolorsaccessible.com/api/are-they", {
-        method: "POST",
-        body: JSON.stringify({ colors: [hex, contrastText] }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          setResult(json.overall);
-        });
+      const response = await fetch(
+        "https://www.aremycolorsaccessible.com/api/are-they",
+        {
+          method: "POST",
+          body: JSON.stringify({ colors: [contrastText, hex] }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const contrastData = await response.json();
+      const contrastScore = contrastData.overall;
+
+      setResult(contrastScore);
     }
     postFetch();
-  });
+  }, [hex, contrastText]);
 
   return <div>Contrast Score: {result}</div>;
 }
