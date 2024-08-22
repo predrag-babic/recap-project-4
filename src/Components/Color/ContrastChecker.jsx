e92395b128afd31dac5832ea36331eda29a5eb64
Predrag <predrag.babic@tactix-sports.com>
2024-08-22 14:00:44 +0200

    add try and catch to the fetch function

--- src/Components/Color/ContrastChecker.jsx
+++ src/Components/Color/ContrastChecker.jsx
@@ -5,20 +5,30 @@ export default function ContrastChecker({ hex, contrastText }) {
 
   useEffect(() => {
     async function postFetch() {
-      const response = await fetch(
-        "https://www.aremycolorsaccessible.com/api/are-they",
-        {
-          method: "POST",
-          body: JSON.stringify({ colors: [contrastText, hex] }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const contrastData = await response.json();
-      const contrastScore = contrastData.overall;
+      try {
+        const response = await fetch(
+          "https://www.aremycolorsaccessible.com/api/are-they",
+          {
+            method: "POST",
+            body: JSON.stringify({ colors: [contrastText, hex] }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-      setResult(contrastScore);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const contrastData = await response.json();
+        const contrastScore = contrastData.overall;
+
+        setResult(contrastScore);
+      } catch (error) {
+        console.error("Error fetching contrast score:", error);
+        setResult("Error fetching contrast score");
+      }
     }
     postFetch();
   }, [hex, contrastText]);
