bba8fb9f47150941cd1fa73f0b0a94ec1022cba4
Predrag <predrag.babic@tactix-sports.com>
2024-08-23 09:29:28 +0200

    add function add and delete theme

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,61 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [newTheme, setNewTheme] = useState("");
 
+  const selectedTheme = themes.find((theme) => theme.id === selectedThemeId);
+
   function handleAddColor(newColor) {
     const colorWithId = { ...newColor, id: nanoid() };
-    setColors([colorWithId, ...colors]);
+    const updatedThemes = themes.map((theme) =>
+      theme.id === selectedThemeId
+        ? { ...theme, colors: [colorWithId, ...theme.colors] }
+        : theme
+    );
+    setThemes(updatedThemes);
   }
 
-  function handleDeleteColor(id) {
-    const filteredColors = colors.filter((color) => color.id !== id);
-    setColors(filteredColors);
+  function handleDeleteColor(colorId) {
+    const updatedThemes = themes.map((theme) =>
+      theme.id === selectedTheme.id
+        ? {
+            ...theme,
+            colors: theme.colors.filter((color) => color.id !== colorId),
+          }
+        : theme
+    );
+    setThemes(updatedThemes);
   }
 
   function handleUpdateColor(updatedColor) {
-    const updatedColors = colors.map((color) =>
-      color.id === updatedColor.id ? updatedColor : color
+    const updatedThemes = themes.map((theme) =>
+      theme.id === selectedThemeId
+        ? {
+            ...theme,
+            colors: theme.colors.map((color) =>
+              color.id === updatedColor.id ? updatedColor : color
+            ),
+          }
+        : theme
     );
-    setColors(updatedColors);
+    setThemes(updatedThemes);
   }
+
+  function handleAddTheme() {
+    const newTheme = {
+      id: nanoid(),
+      name: `New Theme ${themes.length + 1}`,
+      colors: [],
+    };
+    setThemes([newTheme, ...themes]);
+    setSelectedThemeId(newTheme.id);
+  }
+
+  function handleDeleteTheme(themeId) {
+    if (themeId === "t1") return;
+    const updatedThemes = themes.filter((theme) => theme.id !== themeId);
+    setThemes(updatedThemes);
+    setSelectedThemeId(updatedThemes[0].id);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
